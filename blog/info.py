39fc8f8b878c1172473fa8f6ee65f2ab52c2277c
Fadhel Ben Abdi <fadhel.abdi@DOLF.LOCAL>
2023-01-12 15:47:25 +0300

    I add Two page of html

---
+++ blog/info.py
@@ -0,0 +1,16 @@
+
+
+posts = [
+    {
+        'name': 'Sam',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'name': 'Mohammed',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
---
+++ blog/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+#from info import posts
+
+# This code to connecting with html page
+
+
+posts = [
+    {
+        'name': 'Sam',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'name': 'Mohammed',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
+
+def home(request):
+    contx = {
+        'posts': posts
+    }
+    
+    return render(request, 'blog/home.html', contx) # tocontact with html code.
+
+
+def about(request):
+   name = {'name': 'Fadhel'}
+   return render(request, 'blog/about.html', name)
+
+
+# # Create your views here.
