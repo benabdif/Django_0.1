3567d255143855d94f10be6000098a22efddd54d
Fadhel Ben Abdi <fadhel.abdi@DOLF.LOCAL>
2023-01-29 17:11:28 +0300

    biulding a datebase Model

--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,38 @@
 from django.shortcuts import render
+from .models import Post
+
 #from info import posts
 
 # This code to connecting with html page
 
 
-posts = [
-    {
-        'author': 'Sam',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Mohammed',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    },
-    {
-        'author': 'Kaled',
-        'title': 'Blog Post 31',
-        'content': 'Thierd post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Sam',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27, 2018'
+#     },
+#     {
+#         'author': 'Mohammed',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 28, 2018'
+#     },
+#     {
+#         'author': 'Kaled',
+#         'title': 'Blog Post 31',
+#         'content': 'Thierd post content',
+#         'date_posted': 'August 28, 2018'
+#     }
+# ]
 
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
+        #'posts': posts  This code to make dataa come for the tope
     }
     return render(request, 'blog/home.html', context)
 
